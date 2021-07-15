0dba2ffb735e78ca9d5ea7487ad7ae9df8f78784
MBaudot <70977281+MBaudot@users.noreply.github.com>
2021-07-14 22:57:04 -0500

    updated timing
    updated timing & eye candy turned off.

--- stay_awake.cpp
+++ stay_awake.cpp
@@ -2,23 +2,50 @@
 #include <iostream>
 #include <conio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+void delay(int number_of_seconds)
+{
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds);
+}
+
 int main(){
 
-   int x = 1, y = 1;
+   int x = 1, y = 400;
    POINT xypos;
 
    while (1) {
       SetCursorPos(x, y);
-      usleep(30000000);
+      //usleep(10000000);
+      
       if (x == 1) {
-         x = 2;
-         y = 2;
+         x = 400;
+         y = 1;
       } else {
          x = 1;
-         y = 1;
+         y = 400;
       }
+      // //printf("\n");
+      // printf("C:\\Users\\c1121002>");
+      // delay(5);
+      // printf("ping 8.8.8.8\n");
+      // delay(1);
+      // printf("Reply from 8.8.8.8: bytes=32 time=7ms TTL=53\n");
+      // delay(1);
+      // printf("Reply from 8.8.8.8: bytes=32 time=7ms TTL=58\n");
+      // delay(1);
+      // printf("Reply from 8.8.8.8: bytes=32 time=7ms TTL=47\n");
+      // delay(1);
+      // printf("Reply from 8.8.8.8: bytes=32 time=7ms TTL=55\n");
+      // printf("C:\\Users\\c1121002>");
+      delay(5);
+      // system("CLS");
+      // //return 0;
    }
 }
