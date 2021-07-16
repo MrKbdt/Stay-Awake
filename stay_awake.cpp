fef43e0095a803fa17594189abe1e9a3533a90e6
MBaudot <70977281+MrKbdt@users.noreply.github.com>
2021-07-15 20:38:54 -0500

    v1.1 clean
    cleaned up code format
    removed all comments

--- stay_awake.cpp
+++ stay_awake.cpp
@@ -1,57 +1,28 @@
-/*
-The following sample program turns the NUM LOCK light on if it is off.
-The SetNumLock function defined here simulates pressing the NUM LOCK key,
-using keybd_event() with a virtual key of VK_NUMLOCK. It takes a boolean
-value that indicates whether the light should be turned off (FALSE) or on (TRUE).
-
-The same technique can be used for the CAPS LOCK key (VK_CAPITAL) and the
-SCROLL LOCK key (VK_SCROLL).
-*/
-
 #include <windows.h>
 #include <time.h>
 #include <unistd.h>
 
-void SetNumLock(BOOL bState)
-{
+void SetNumLock(BOOL bState) {
    BYTE keyState[256];
-
    GetKeyboardState((LPBYTE)&keyState);
-   if ((bState && !(keyState[VK_NUMLOCK] & 1)) ||
-       (!bState && (keyState[VK_NUMLOCK] & 1)))
-   {
-      // Simulate a key press
-      keybd_event(VK_NUMLOCK,
-                  0x45,
-                  KEYEVENTF_EXTENDEDKEY | 0,
-                  0);
-
-      // Simulate a key release
-      keybd_event(VK_NUMLOCK,
-                  0x45,
-                  KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
-                  0);
+   if ((bState && !(keyState[VK_NUMLOCK] & 1)) || (!bState && (keyState[VK_NUMLOCK] & 1))) {
+      keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+      keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
    }
 }
 
-int main()
-{
-
-int toggle = 0;
+int main(){
+   int toggle = 0;
    
-   while (1)
-   {
-      if (toggle == 0)
-      {
+   while (1) {
+      if (toggle == 0) {
          toggle = 1;
          SetNumLock(TRUE);
       }
-      else
-      {
+      else {
          toggle = 0;
          SetNumLock(FALSE);
       }
-      // 
       usleep(60000000);
    } 
 }
