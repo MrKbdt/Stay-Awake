a0e4e1f036810ece3f9a6f615890c3e27d1c08dc
MBaudot <70977281+MrKbdt@users.noreply.github.com>
2021-07-15 23:23:58 -0500

    v1.11 added documentation
    v1.11 added documentation

--- stay_awake.cpp
+++ stay_awake.cpp
@@ -1,3 +1,20 @@
+/*
+
+The following sample program turns the NUM LOCK light on if it is off.
+The SetNumLock function defined here simulates pressing the NUM LOCK key,
+using keybd_event() with a virtual key of VK_NUMLOCK. It takes a boolean
+value that indicates whether the light should be turned off (FALSE) or on (TRUE).
+The same technique can be used for the CAPS LOCK key (VK_CAPITAL) and the
+SCROLL LOCK key (VK_SCROLL).
+
+keybd_event function (winuser.h)
+https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-keybd_event
+
+Virtual-Key Codes
+https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+
+*/
+
 #include <windows.h>
 #include <time.h>
 #include <unistd.h>
